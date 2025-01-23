ee534699e249d2b84def4f6f2cedcc991bbcece2
Melo <meiko_ooo@163.com>
2025-01-23 16:01:38 +0800

    feat:依赖位置调整

--- sys-client/src/main/java/com/young/hub/sys/client/api/UserClient.java
+++ sys-client/src/main/java/com/young/hub/sys/client/api/UserClient.java
@@ -1,6 +1,7 @@
 package com.young.hub.sys.client.api;
 
 import cn.young.boot.common.ResponseData;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author Mole. meiko_ooo@163.com
  * @since 2025/1/22 00:00
  */
+@FeignClient(value = "young-sys-api", contextId = "UserClient", path = "/api")
 public interface UserClient {
 
     @GetMapping(value = "query-by-username")
