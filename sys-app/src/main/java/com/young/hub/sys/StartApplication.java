f0ae4b6025558795d4ad3a1b5ce35733b177bc57
Melo <meiko_ooo@163.com>
2025-01-23 14:35:58 +0800

    feat:新增nacos配置

--- sys-app/src/main/java/com/young/hub/sys/StartApplication.java
+++ sys-app/src/main/java/com/young/hub/sys/StartApplication.java
@@ -2,6 +2,7 @@ package com.young.hub.sys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  */
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class StartApplication {
     public static void main(String[] args) {
         SpringApplication.run(StartApplication.class);
